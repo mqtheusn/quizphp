3faca312bcb97f958131fb6e8998eb437433a4e3
mqtheusn <matheuszor14@gmail.com>
2024-10-23 22:11:35 -0300

    quizphp

---
+++ quiz1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Quiz Jojo's Bizarre Adventures</title>
+</head>
+	<body style="margin: 10px;">
+	<form name="Quiz" method="post" action="quiz2.php">
+	<label><h2>Primeira pergunta!!!</h2></label> <br> <br>
+	<label>Qual é a ordem correta de protagonistas?</label><br><br>
+	<input type="submit" class="btn-btn-dark" name="opção 1" value="Joseph / Jonathan / Josuke / Jotaro / Giorno / Jolyne"><br><br> 
+	<input type="submit" name="opção 2" value="Jotaro / Joseph / Josuke / Jolyne / Giorno / Jonathan"><br><br>
+	<input type="submit" name="opção 3" value="Jonathan / Joseph / Jotaro / Giorno / Jolyne / Josuke"><br><br>
+	<input type="submit" name="opção 4" value="Jonathan / Joseph / Jotaro / Josuke / Giorno / Jolyne"><br><br>
+</form>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</html>
